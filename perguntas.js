fe609ac524204820e8a9ffbe2c7307d727f38704
Cicerogab <161970787+Cicerogab@users.noreply.github.com>
2024-11-01 12:50:49 -0300

    Add files via upload
    Página em andamento.

---
+++ perguntas.js
@@ -0,0 +1,40 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'Sigla para HyperText Markup Language — Linguagem de Marcação de Hipertexto'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'História',
+    'Onde nasceu Justino Marcondes rangel?',
+    ' Guaratinguetá, São Paulo, Brasil'
+)
+
+criaCartao( 
+    'Programação',
+    'O que é CSS?',
+    'CSS é a sigla para o termo em inglês Cascading Style Sheets'
+)
