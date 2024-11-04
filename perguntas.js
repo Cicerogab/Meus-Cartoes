b76f057bf154e91028468225f815d0db9101cd6a
Cicerogab <161970787+Cicerogab@users.noreply.github.com>
2024-11-04 10:25:48 -0300

    Add files via upload
    Em desenvolvimento.

--- perguntas.js
+++ perguntas.js
@@ -1,13 +1,14 @@
-criaCartao(
-    'História',
-    'O que significa halloween?',
-    ' Halloween (ou Dia das Bruxas)A celebração vem de um antigo festival pagão celebrado pelos celtas há mais de 2.000 anos nas regiões do Reino Unido, Irlanda e noroeste da França, chamado Samhain.'
-)
 
 criaCartao(
     'Língua Portuguesa (nome)',
     ' O que significa o nome Cícero?',
     'Cícero é um nome com origem no latim Cicero. Na realidade ele era considerado um cognome romano (um apelido, uma alcunha), da palavra cicer.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Gabriel?',
+    'Gabriel: Significa “homem de Deus”, “homem forte de Deus”, “fortaleza de Deus”, “mensageiro de Deus”.'
 )
 
 criaCartao(
@@ -26,6 +27,12 @@ criaCartao(
     'Língua Portuguesa (nome)',
     ' O que significa o nome Jessé?',
     'Jessé: Significa “rei”, "presente de Deus", "Deus existe"'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Kléber?',
+    'Kléber: Significa “colador de cartazes”. Kléber é um nome de origem germânica, que vem de Klaeber, cujo significado é “colador de cartazes”. Sendo uma profissão era, inicialmente, o sobrenome das pessoas que praticavam esse ofício.'
 )
 
 criaCartao(
@@ -66,47 +73,127 @@ criaCartao(
 
 criaCartao(
     'Língua Portuguesa (nome)',
+    ' O que significa o nome Luciene?',
+    'Luciene: Significa “de Lúcio”, “pertencente a Lúcio”, “da natureza do iluminado”, "a luminosa"; "luminosa e graciosa", "luminosa e cheia de graça".'
+)
+
+
+criaCartao(
+    'Língua Portuguesa (nome)',
     ' O que significa o nome Renata?',
     'Renata: Significa "renascida", "ressuscitada" ou "nascida pela segunda vez".'
 )
 
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Mônica?',
+    'Mônica: Significa “única”, “só”, “solitária”, “conselheira”. Forte e elegante, o nome feminino Mônica apresenta duas possibilidades de origem e significados diversos associados a ele'
 )
 
 criaCartao(
-    'Programação',
-    'O que é HTML?',
-    'Sigla para HyperText Markup Language — Linguagem de Marcação de Hipertexto'
-)
-criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Rafaela?',
+    'Rafaela: Significa “Deus curou” ou “curada por Deus”. Rafaela é um bonito nome de origem hebraica que está amplamente relacionado à figura do arcanjo Rafael.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Amanda?',
+    'Amanda: Significa “digna de amor”, “amável”, “aquela que deve ser amada”.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Cláudia?',
+    'Forte, imponente e associado com a nobreza do Império Romano, o nome Cláudia é a versão feminina do nome Cláudio, que tem origem a partir do latim Claudius.'
 )
 
 criaCartao(
-    'História',
-    'Onde nasceu Justino Marcondes rangel?',
-    ' Guaratinguetá, São Paulo, Brasil'
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Ruth?',
+    'Ruth: Significa “amiga”, “companheira”.É a versão original em hebraico do nome Rute, que deriva do termo Reut, que quer dizer "amigo" e por extensão pode significar ainda "companheira".'
 )
 
-criaCartao( 
-    'Programação',
-    'O que é CSS?',
-    'CSS é a sigla para o termo em inglês Cascading Style Sheets'
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Patrícia?',
+    'Patrícia: Significa “nobre”, “de classe nobre”, “da mesma pátria” ou “compatriota”.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Roberta?',
+    'Roberta: Significa “aquela que a glória tornou famosa”, “brilhante na glória” ou “famosa e gloriosa”.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Ana?',
+    'Ana: Significa "graciosa" ou "cheia de graça". O nome Ana vem do original em hebraico Hannah, mais tarde do latim Anna, que quer dizer "graciosa, cheia de graça".'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Verônica?',
+    'Verônica: Significa “portadora da vitória”, "imagem verdade”.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Maria?',
+    'Maria: Significa “senhora soberana”, “vidente” ou “a pura”.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Sirlendi?',
+    'Sirlendi: Significa “inflada com vinho” ou “pasto verdejante”, “campina luminosa”, “clareira brilhante”'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Ângela?',
+    'Ângela: Significa “mensageira”, “anjo”'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Roselene?',
+    'Roselene: Variação do nome Rosilene. Significa "pequena rosa" ou “espécie famosa”.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Bete?',
+    'Bete: Significa “Deus é juramento”, “Deus é abundância” ou “casa da obediência”.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Leila?',
+    'Leila: Significa “escura”, “noite”, "escura como a noite", "o cheiro inebriante do vinho", “a embriaguez dos poetas”.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Vera?',
+    'Vera: Significa “verdadeira”, “sincera”, “franca”, “a que tem fé”. Um nome curto, elegante e com significados fortes, Vera tem origem a partir do latim verus, vera ou verum. Estas palavras são as raízes para “verdade” e “verdadeiro”.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Márcia?',
+    'Márcia: Significa “guerreira”, “marcial”, “dedicada a Marte”. Forte, imponente e com significados lendários, o nome Márcia surge como uma variante feminina do popular nome Márcio.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Bruna?',
+    'Bruna: Significa “morena”, "bronzeada", "marrom"; "da cor do fogo". Com uma sonoridade bela e significados históricos, o nome Bruna se tornou um dos preferidos entre os brasileiros e italianos.'
+)
+
+criaCartao(
+    'Língua Portuguesa (nome)',
+    ' O que significa o nome Diogo?',
+    'Diogo: Significa “aquele que vem do calcanhar”, “aquele que doutrina, aquele que ensina”. Tem origem no nome espanhol Diego, que tem o mesmo sentido de Jacó (Iago) ou, até mesmo, de Tiago. Significa “aquele que vem do calcanhar”.'
 )
